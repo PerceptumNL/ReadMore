f89009fb08fb69717677de2860dea85aa6daca3e
mslatour <sanderlatour@gmail.com>
2014-07-22 18:21:35 +0200

    Set absolute path to STATIC ROOT

--- readmore/settings-production.py
+++ readmore/settings-production.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/app/staticfiles'
 STATIC_URL = '/static/'
 
 # Quick-start development settings - unsuitable for production
