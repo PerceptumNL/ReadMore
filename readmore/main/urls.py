66cee435c1933d552b8ab1d33dcb82c46c69c3f9
RCJurriaans <robrechtjurriaans@gmail.com>
2014-07-22 17:35:27 +0200

    Began Base html file, added views for categories and category page

--- readmore/main/urls.py
+++ readmore/main/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
-    url(r'^$', 'readmore.main.views.home', name='home')
+    url(r'^$', 'readmore.main.views.home', name='home'),
+    url(r'^categories/$', 'readmore.main.views.categories', name='categories'),
+    url(r'^category/(?P<category_id>\d+)/?$', 'readmore.main.views.category', name='category'),
 )
--- readmore/main/views.py
+++ readmore/main/views.py
@@ -3,4 +3,14 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
+    return render(request, 'base.html', {
+        "crtCategory": {"name": "test", "pk": 0},
+        "categories": [{"name": "test", "pk": 0},{"name": "test2", "pk": 1}]
+       
+    })
+
+def categories(request):
     return HttpResponse("ReadMore")
+
+def category(request, category_id):
+    return HttpResponse(category_id)    
