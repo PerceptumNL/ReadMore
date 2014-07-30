ac5e6f0d8e8ba06800e30e54035b7cdcd4ac4be1
mslatour <sanderlatour@gmail.com>
2014-07-30 22:57:45 +0200

    Added a wordclick event, triggered when clicking on a word in the text.

--- readmore/widgets/articleviewer/static/js/widget_articleviewer.js
+++ readmore/widgets/articleviewer/static/js/widget_articleviewer.js
@@ -4,14 +4,61 @@ $.widget( "readmore.articleviewer", {
 		article: undefined
 	},
 	_create: function(){
-		_self = this
+		var _self = this
 		api_call(
 			this.options.article,
 			{},
 			'GET',
 			function(data, xhr){
 				_self.element.html(data['body']);
+				_self.postprocess()
 			}
 		);
 	},
+	postprocess: function(){
+		/* Make all words clickable.
+		 * This is done by the following steps:
+		 *   1. All textnodes (in all depths) are collected breadth-first.
+		 *   2. From the text in each textnode, a new string is extracted where
+		 *      each word is wrapped by a <span> element with the class ".word"
+		 *   3. The new string is placed in a new <span> element, which is inserted
+         *      in the DOM just before the textnode
+         *   4. The textnode itself is removed
+         *   5. Each element with the ".word" class gets an onClick event
+		 *
+		 * Clicking a word triggers the "wordclick" event, containing data["word"].
+		 */
+		var _self = this
+		textnodes = []
+
+		// Set traverse list to  toplevel child nodes
+		nodes = $.makeArray($(this.element).contents())
+
+		// While there are still nodes to traverse
+		while(nodes.length > 0){
+			// Pick the first node
+			node = nodes.shift();
+			if(node.nodeType == 3){
+				// If it's a textnode, save
+				textnodes.push(node);
+			}else{
+				// Else, add child nodes to traverse list
+				nodes = nodes.concat($.makeArray($(node).contents()))
+			}
+		}
+		// Replace each word
+		$(textnodes).each(function(){
+			// Create string containing new HTML
+			content = this.textContent.replace(/(\w+)/g, "<span class='word'>$1</span>")
+			// Add new HTML to DOM
+			$("<span>"+content+"</span>").insertBefore(this)
+			// Remove old textnode
+			$(this).remove()
+		})
+		// Add onclick events on each word
+		$(".word").click(function(e){
+			// Trigger wordclick event, providing the word that was clicked on.
+			_self._trigger("wordclick", e, {"word": $(this).text()})
+		})
+	}
 })
