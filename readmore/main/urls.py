a576963e77b8cbf32362891edce1b12261526e25
RCJurriaans <robrechtjurriaans@gmail.com>
2014-08-01 15:17:09 +0200

    Added registration option in bottom left corner

--- readmore/main/urls.py
+++ readmore/main/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'readmore.content.views.index', name='home'),
-        url(r'^accounts/', include('allauth.urls')),
+    url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+    url(r'^accounts/', include('allauth.urls')),
 )
