25df013726653f00f54824d9f7ccbe1302353c9b
RCJurriaans <robrechtjurriaans@gmail.com>
2014-08-04 17:06:08 +0200

    Work on Accounts/Badges connection

--- readmore/main/admin.py
+++ readmore/main/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from models import *
 
 # Register your models here.
+class CounterBadgeInline(admin.StackedInline):
+    model = CounterBadge
+    extra = 0
+    
+class BadgeAdmin(admin.ModelAdmin):
+    base_model = Badge
+    inlines = [
+        CounterBadgeInline,
+    ]
+    
+    
+admin.site.register(Badge, BadgeAdmin)    
+admin.site.register(Statistics)
