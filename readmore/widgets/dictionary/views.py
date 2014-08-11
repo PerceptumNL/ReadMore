5dd85254c077a5f37bd7ed8347fca3928cf5aa1c
Elise <koster.elise@gmail.com>
2014-08-11 15:14:39 +0200

    More work on dictionary widget; restyled barrier page (needs login-check)

--- readmore/widgets/dictionary/views.py
+++ readmore/widgets/dictionary/views.py
@@ -6,19 +6,39 @@ import json
 # Create your views here.
 def process(request):
     word = request.GET.get('word',"No word given")
-    print "word: " + str(word)
+    
     api = WiktionaryAPI(languages=['nld'])
-    #mainterm = api.get_info(word)[0].main_term
-    #print "main: " + str(mainterm)
+
+    # Get list of Terms and Forms for word
     info = api.get_info(word)
-    betekenis = [str(word) + " >> " + str(word)]
-    betekenis.append('<ul>')
-    print 'inf ' + str(info)
-    for meaninglist in info:
-        allmeanings = meaninglist.meanings
-        for singlemeaning in allmeanings:
-            betekenis.append('<li>' + singlemeaning.definition + '</li>')
-    betekenis.append('</ul>')
-    print betekenis
+    print "info: " + str(info)
+    mainterm = []
+
+    # For every Term and Form
+    for types in info:
+    	# If types is a Form, get the main term and append
+    	if isinstance(types, TermForm):
+    		term = api.get_info(types.main_term)
+    		mainterm.append(term)
+    		print "term: " + str(term)
+    	# If types is a term, append
+        else:
+    	    mainterm.append(info)
+    print "mainterm: " + str(mainterm)
+    
+    betekenis = [str(word) + " >> "]
+
+    for termlist in mainterm:
+    	for term in termlist:
+    		print "term: " + str(term.entry)
+    		betekenis.append(term.entry)
+    		meaninglist = term.meanings
+    		print "means: " + str(meaninglist)
+    		betekenis.append('<ul>')
+    		for meaning in meaninglist:
+    			print "onemeaning: " + str(meaning)
+    			betekenis.append('<li>' + str(meaning.definition) + '</li>')
+    		betekenis.append('</ul>')
+
     return HttpResponse(json.dumps({'word':betekenis}),
             content_type='application/json')
