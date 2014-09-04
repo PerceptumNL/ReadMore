5932d3c4fe2497913ce0dd509b1d8ebe6cf9c021
mslatour <sanderlatour@gmail.com>
2014-09-04 15:26:24 +0200

    Created carddeck of dictionary widgets

--- readmore/widgets/dictionary/views.py
+++ readmore/widgets/dictionary/views.py
@@ -5,45 +5,49 @@ import json
 
 # Create your views here.
 def process(request):
+    return HttpResponse(json.dumps([{"data": {"term_category": "noun", "synonyms": {"0": ["canape", "divan", "sofa"], "2": ["zandbank"]}, "word": "bank"}, "type": "DictSynonymCard"}, {"data": {"category": "noun", "meanings": {"0": {"definition": "een meubelstuk met zitplaats voor meer dan een persoon", "example": "Ze zaten op de '''bank''' naar de tv te kijken."}, "1": {"definition": "een financiele instelling", "example": "Vader was naar de '''bank''' om te praten over een lening."}, "2": {"definition": "een ondiepte in het water", "example": "De boot was op een '''bank''' vastgelopen."}, "3": {"definition": "gebouw waarin een financiele instelling gevestigd is", "example": ""}, "4": {"definition": "een opslagsysteem voor gegevens of voorwerpen b.v. beeldbank, bloedbank, boekenbank, kennisbank, spermabank", "example": ""}, "5": {"definition": "harde aardlaag", "example": ""}, "6": {"definition": "donkere laag of streep van wolken aan de horizon.", "example": ""}, "7": {"definition": "werktafel b.v. draaibank etc.", "example": ""}}, "word": "bank"}, "type": "DictTermCard"}, {"data":{"antonyms":{"1":["honesty", "trust"]}, "word":"bank", "term_category":"noun"}, "type": "DictAntonymCard"}]), content_type='application/json')
     word = request.GET.get('word',"No word given")
     api = WiktionaryAPI(languages=['nld'])
 
     # Get list of Terms and Forms for word
-    try:
-        info = api.get_info(word)
-    except Exception:
-        info = []
-    mainterm = []
+    info = api.get_info(word)
 
-    # For every Term and Form
-    if(info != []):
-        for types in info:
-        	# If types is a Form, get the main term and append
-            if isinstance(types, TermForm):
-                # TODO: Use types.form2text() to get human-readable text
-                term = api.get_info(types.main_term)
-                mainterm.append(term)
-        	# If types is a term, append
-            else:
-                mainterm.append(info)
-
-        for termlist in mainterm:
-            for term in termlist:
-                if(isinstance(term, TermForm)):
-                    pass
-                else:
-                    betekenis = ["<h3 class='title'>" + str(word) + " >> " + str(term.entry) + "</h3>"]
-                    meaninglist = term.meanings
-                    betekenis.append('<ul>')
-                    for meaning in meaninglist:
-                        definition = WiktionaryParser.clean_wikitext(
-                                meaning.definition)
-                        betekenis.append('<li>%s</li>' % (
-                            definition.encode('ascii',
-                                'xmlcharrefreplace')))
-                    betekenis.append('</ul>')
-
-    else:
-        betekenis = "<h3 class='title'>Woordenboek</h3><p><span class='notfound'>" + str(word) + "</span> is niet gevonden!</p>"
-    return HttpResponse(json.dumps({'word':betekenis}),
+    cards = []
+    for term in info:
+        card = {}
+        if isinstance(term, Term):
+            card['type'] = 'DictTermCard'
+            card['data'] = {
+                'category': term.category_description,
+                'word': word,
+                'meanings': {}
+            }
+            synonyms = {}
+            antonyms = {}
+            for index, meaning in enumerate(term.meanings):
+                definition = WiktionaryParser.clean_wikitext(
+                        meaning.definition)
+                example = WiktionaryParser.clean_wikitext(
+                        meaning.example)
+                card['data']['meanings'][index] = {
+                    'definition': definition,
+                    'example': example}
+                if meaning.synonyms:
+                    synonyms[index] = meaning.synonyms
+                if meaning.antonyms:
+                    antonyms[index] = meaning.antonyms
+            # Create corresponding synonyms card
+            cards.append({'type': 'DictSynonymCard',
+                'data': {
+                    'word': word,
+                    'term_category': card['data']['category'],
+                    'synonyms': synonyms}})
+            # Create corresponding synonyms card
+            cards.append({'type': 'DictAntonymCard',
+                'data': {
+                    'word': word,
+                    'term_category': card['data']['category'],
+                    'antonyms': antonyms}})
+        cards.append(card)
+    return HttpResponse(json.dumps(cards).encode('ascii', 'xmlcharrefreplace'),
             content_type='application/json')
