6b8a49b05adb86b14f32afdf74d60bad399e53cc
mslatour <sanderlatour@gmail.com>
2014-09-21 23:51:38 +0200

    Code in position for handling form submissions.

--- readmore/widgets/askteacher/views.py
+++ readmore/widgets/askteacher/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseBadRequest
 import json
 
 # Create your views here.
 def process(request):
-    cards = [{'type':'FormCard', 'data': {'title': 'Vraag aan de docent'}}]
-    return HttpResponse(json.dumps(cards).encode('ascii', 'xmlcharrefreplace'),
-            content_type='application/json')
+    if request.method == "POST":
+        pass
+    elif request.method == "GET":
+        cards = [{'type':'FormCard', 'data': {'title': 'Vraag aan de docent'}}]
+        return HttpResponse(json.dumps(cards).encode('ascii', 'xmlcharrefreplace'),
+                content_type='application/json')
+    else:
+        return HttpResponseBadRequest()
+
