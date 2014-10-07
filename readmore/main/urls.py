7027ba7a7cb8bb66d1624cda18394d523094467f
RCJurriaans <robrechtjurriaans@gmail.com>
2014-10-07 16:09:04 +0200

    merging

--- readmore/main/urls.py
+++ readmore/main/urls.py
@@ -7,6 +7,7 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}),
     url(r'^accounts/profile/$', 'readmore.main.views.profileSelf', name='profile'), 
     url(r'^accounts/login/$', 'readmore.main.views.login', name='login2'),
+    url(r'^accounts/signup/$', 'readmore.main.views.login', name='login3'),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^profile/(?P<user_id>\w+)$', 'readmore.main.views.profile', name='profile'),
     url(r'^about/', 'readmore.content.views.about', name='about')
