4dcc6197b7083486d31841f4f629a6ad85ea4846
mslatour <sanderlatour@gmail.com>
2015-05-28 17:29:34 +0200

    Added kidsweek content

--- readmore/content/admin.py
+++ readmore/content/admin.py
@@ -22,6 +22,7 @@ class CategoryAdmin(PolymorphicParentModelAdmin):
         (RSSCategory, RSSCategoryAdmin),
         (WikiCategory, WikiCategoryAdmin),
         (SevenDaysCategory, SevenDaysCategoryAdmin),
+        (KidsWeekCategory, SevenDaysCategoryAdmin),
         (Category, RegularCategoryAdmin)
     )
 
