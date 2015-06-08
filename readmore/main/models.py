f1648a1b9d19edeee79215784bcd3c6f45cfc262
mslatour <sanderlatour@gmail.com>
2015-06-08 20:56:19 +0200

    Added code generation for groups

--- readmore/main/models.py
+++ readmore/main/models.py
@@ -15,6 +15,8 @@ from polymorphic import PolymorphicModel
 import allauth.app_settings
 from allauth.socialaccount import providers
 
+from readmore.teacher.helpers import generate_password
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, unique=True)
     badges = models.ManyToManyField('Badge', blank=True, related_name='users')
@@ -59,12 +61,35 @@ class Group(models.Model):
     title = models.CharField(max_length=255)
     leader = models.ForeignKey(User, null=True, blank=True)
     institute = models.ForeignKey('Institute', null=True, blank=True)
+    code = models.CharField(max_length=255, blank=True)
 
     def __unicode__(self):
         return u'Group of %s' % (self.leader,)
 
     def __str__(self):
         return unicode(self).encode('utf-8')
+
+    def pk_hash(self):
+        pk_code = "%03d" % (self.pk*17,)
+        pk_code = pk_code[-3:]
+        return pk_code
+
+    def generate_new_code(self):
+        done=False
+        code = ""
+        while not done:
+            pkcode = self.pk_hash()
+            groupcode = generate_password('-')
+            code = "%s-%s" % (pkcode, groupcode)
+            grouplist = Group.objects.filter(code=code)
+            if not grouplist:
+                done=True
+        return code
+
+    def save(self, *args, **kwargs):
+        if not self.code:
+            self.code = self.generate_new_code()
+        return super(Group, self).save(*args, **kwargs)
 
 
 class Event(PolymorphicModel):
