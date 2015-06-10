ccf4ca1c49032ad94783625eac57c4ca01d3c16d
tdoolan <tim.doolan@gmail.com>
2015-06-10 19:18:21 +0200

    Fixed issue #14

--- readmore/teacher/static/js/teacher_student.js
+++ readmore/teacher/static/js/teacher_student.js
@@ -9,27 +9,18 @@ function initialize_student(student_id){
         if(data.articles_read.length>0){
             $.each( data.articles_read, function( index, value ){
                 var block = $('<div>');
-                block.addClass('article-block');
-                block.addClass('link-div');
-                // Link
+                block.addClass('articleTile');
+                block.attr("style", "background-image: url('"+value.image+"');");
                 var link = $("<a>");
                 link.attr('href', value.url);
                 block.append(link);
-                // Image
-                var image_div = $("<div>");
-                image_div.addClass("article-image");
-                var image = $("<img>");
-                image.attr('src', value.image);
-                image_div.append(image);
-                block.append(image_div);
-                // Title
-                var title_div = $("<div>");
-                title_div.addClass("article-title");
-                var title = $("<h6>");
-                title.html(value.title);
-                title_div.append(title);
-                block.append(title_div);
                 
+                var title = $('<div>');
+                title.addClass("articleTitle");
+                var par = $('<p>');
+                par.html(value.title);
+                title.html(par);
+                block.append(title);
                 $( "#articles_read").append(block);
             });
         } else{
@@ -59,22 +50,30 @@ function initialize_student(student_id){
         
         if (!$.isEmptyObject(data.rating)) {
             var block = $('<div>');
+            block.addClass('articleTile');
             block.addClass('rating-block');
+            block.attr("style", "background-image: url('"+data.rating.image+"');");
             var link = $("<a>");
-            link.attr('href', data.rating.link);
-            link.html(data.rating.title);
+            link.attr('href', data.rating.url);
+            block.append(link);
             
+            var title = $('<div>');
+            title.addClass("articleTitle");
+            var par = $('<p>');
+            par.html(data.rating.title);
+            title.html(par);
+            block.append(title);
+            
+            var rating = $('<h2>');
             for (i = 0; i < 5; i++) {
                 if (i < data.rating.rating) {
-                    link.append('<i class="fa fa-star">')
+                    rating.append('<i class="fa fa-star">')
                 } else {
-                    link.append('<i class="fa fa-star-o">')
+                    rating.append('<i class="fa fa-star-o">')
                 }
             }
+            block.append(rating);
             
-            var title = $("<h5>");
-            title.html(link);
-            block.append(title);
             $( "#rating").append(block);
         } else {
             var title = $("<h6 style='margin: 8px 8px 8px 24px'>");
@@ -84,22 +83,30 @@ function initialize_student(student_id){
         
         if (!$.isEmptyObject(data.difficulty)) {
             var block = $('<div>');
+            block.addClass('articleTile');
             block.addClass('rating-block');
+            block.attr("style", "background-image: url('"+data.difficulty.image+"');");
             var link = $("<a>");
-            link.attr('href', data.difficulty.link);
-            link.html(data.difficulty.title);
+            link.attr('href', data.difficulty.url);
+            block.append(link);
             
+            var title = $('<div>');
+            title.addClass("articleTitle");
+            var par = $('<p>');
+            par.html(data.difficulty.title);
+            title.html(par);
+            block.append(title);
+            
+            var rating = $('<h2>');
             for (i = 0; i < 5; i++) {
                 if (i < data.difficulty.rating) {
-                    link.append('<i class="fa fa-star">')
+                    rating.append('<i class="fa fa-star">')
                 } else {
-                    link.append('<i class="fa fa-star-o">')
+                    rating.append('<i class="fa fa-star-o">')
                 }
             }
+            block.append(rating);
             
-            var title = $("<h5>");
-            title.html(link);
-            block.append(title);
             $( "#difficulty").append(block);
         } else {
             var title = $("<h6 style='margin: 8px 8px 8px 24px'>");
@@ -107,6 +114,35 @@ function initialize_student(student_id){
             $( "#difficulty").append(title);
         }
         
-        //data.progress
+        var graph = d3.select("#graph"),
+            WIDTH = 450,
+            HEIGHT = 350,
+            MARGINS = {
+                top: 20,
+                right: 20,
+                bottom: 50,
+                left: 50
+            },
+            xScale = d3.scale.linear().range([MARGINS.left, WIDTH - MARGINS.right]).domain([-4, -1]),
+            yScale = d3.scale.linear().range([HEIGHT - MARGINS.top, MARGINS.bottom]).domain([0, 10]),
+            yAxis = d3.svg.axis().scale(yScale).tickFormat(d3.format("d")).orient('left');
+        graph.append("svg:g")
+            .attr("transform", "translate(" + (MARGINS.left) + ",0)")
+            .call(yAxis);
+        
+        graph.selectAll("circle")
+           .data(data.progress)
+           .enter()
+           .append("circle")
+           .attr("cx", function(d, i) {
+                return xScale(-4 + i);
+           })
+           .attr("cy", function(d, i) {
+                return yScale(d);
+           })
+           .attr("r", 5)
+           .attr("fill", "blue");
+        
+        
     });
 }
