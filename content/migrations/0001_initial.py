e9c1237fc3147d9ab8e932b2ff392f315530843b
mslatour <sanderlatour@gmail.com>
2015-08-28 00:09:39 +0200

    Upgraded to Django 1.8+, switched migrations standard.

--- content/migrations/0001_initial.py
+++ content/migrations/0001_initial.py
@@ -1,102 +1,145 @@
 # -*- coding: utf-8 -*-
-from south.utils import datetime_utils as datetime
-from south.db import db
-from south.v2 import SchemaMigration
-from django.db import models
+from __future__ import unicode_literals
+
+from django.db import models, migrations
 
 
-class Migration(SchemaMigration):
+class Migration(migrations.Migration):
 
-    def forwards(self, orm):
-        # Adding model 'Category'
-        db.create_table(u'content_category', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('polymorphic_ctype', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'polymorphic_content.category_set', null=True, to=orm['contenttypes.ContentType'])),
-            ('parent', self.gf('django.db.models.fields.related.ForeignKey')(blank=True, related_name='children', null=True, to=orm['content.Category'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('image', self.gf('django.db.models.fields.URLField')(max_length=255, null=True, blank=True)),
-        ))
-        db.send_create_signal(u'content', ['Category'])
+    dependencies = [
+        ('contenttypes', '0002_remove_content_type_name'),
+    ]
 
-        # Adding model 'WikiCategory'
-        db.create_table(u'content_wikicategory', (
-            (u'category_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['content.Category'], unique=True, primary_key=True)),
-            ('identifier', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('identifier_type', self.gf('django.db.models.fields.CharField')(default='title', max_length=6, blank=True)),
-            ('wiki_type', self.gf('django.db.models.fields.CharField')(default='14', max_length=3)),
-        ))
-        db.send_create_signal(u'content', ['WikiCategory'])
-
-        # Adding model 'Article'
-        db.create_table(u'content_article', (
-            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
-            ('polymorphic_ctype', self.gf('django.db.models.fields.related.ForeignKey')(related_name=u'polymorphic_content.article_set', null=True, to=orm['contenttypes.ContentType'])),
-            ('category', self.gf('django.db.models.fields.related.ForeignKey')(related_name='articles', to=orm['content.Category'])),
-            ('title', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('body', self.gf('django.db.models.fields.TextField')(null=True, blank=True)),
-        ))
-        db.send_create_signal(u'content', ['Article'])
-
-        # Adding model 'WikiArticle'
-        db.create_table(u'content_wikiarticle', (
-            (u'article_ptr', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['content.Article'], unique=True, primary_key=True)),
-            ('identifier', self.gf('django.db.models.fields.CharField')(max_length=255)),
-            ('identifier_type', self.gf('django.db.models.fields.CharField')(default='title', max_length=6, blank=True)),
-        ))
-        db.send_create_signal(u'content', ['WikiArticle'])
-
-
-    def backwards(self, orm):
-        # Deleting model 'Category'
-        db.delete_table(u'content_category')
-
-        # Deleting model 'WikiCategory'
-        db.delete_table(u'content_wikicategory')
-
-        # Deleting model 'Article'
-        db.delete_table(u'content_article')
-
-        # Deleting model 'WikiArticle'
-        db.delete_table(u'content_wikiarticle')
-
-
-    models = {
-        u'content.article': {
-            'Meta': {'object_name': 'Article'},
-            'body': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
-            'category': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'articles'", 'to': u"orm['content.Category']"}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_content.article_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        u'content.category': {
-            'Meta': {'object_name': 'Category'},
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'image': ('django.db.models.fields.URLField', [], {'max_length': '255', 'null': 'True', 'blank': 'True'}),
-            'parent': ('django.db.models.fields.related.ForeignKey', [], {'blank': 'True', 'related_name': "'children'", 'null': 'True', 'to': u"orm['content.Category']"}),
-            'polymorphic_ctype': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "u'polymorphic_content.category_set'", 'null': 'True', 'to': u"orm['contenttypes.ContentType']"}),
-            'title': ('django.db.models.fields.CharField', [], {'max_length': '255'})
-        },
-        u'content.wikiarticle': {
-            'Meta': {'object_name': 'WikiArticle', '_ormbases': [u'content.Article']},
-            u'article_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['content.Article']", 'unique': 'True', 'primary_key': 'True'}),
-            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'identifier_type': ('django.db.models.fields.CharField', [], {'default': "'title'", 'max_length': '6', 'blank': 'True'})
-        },
-        u'content.wikicategory': {
-            'Meta': {'object_name': 'WikiCategory', '_ormbases': [u'content.Category']},
-            u'category_ptr': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['content.Category']", 'unique': 'True', 'primary_key': 'True'}),
-            'identifier': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
-            'identifier_type': ('django.db.models.fields.CharField', [], {'default': "'title'", 'max_length': '6', 'blank': 'True'}),
-            'wiki_type': ('django.db.models.fields.CharField', [], {'default': "'14'", 'max_length': '3'})
-        },
-        u'contenttypes.contenttype': {
-            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
-            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
-            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
-            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
-        }
-    }
-
-    complete_apps = ['content']
+    operations = [
+        migrations.CreateModel(
+            name='Article',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('body', models.TextField(null=True, blank=True)),
+                ('image', models.URLField(null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+        migrations.CreateModel(
+            name='Category',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('color', models.CharField(default=b'#f3f3f3', max_length=50, blank=True)),
+                ('order', models.IntegerField(default=0)),
+                ('image', models.CharField(max_length=255, null=True, blank=True)),
+            ],
+            options={
+                'verbose_name_plural': 'Categories',
+            },
+        ),
+        migrations.CreateModel(
+            name='ContentSource',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('link', models.URLField(max_length=255)),
+                ('logo', models.CharField(max_length=255, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='RSSArticle',
+            fields=[
+                ('article_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='content.Article')),
+                ('publication_date', models.DateTimeField()),
+                ('identifier', models.URLField(max_length=255)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('content.article',),
+        ),
+        migrations.CreateModel(
+            name='RSSCategory',
+            fields=[
+                ('category_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='content.Category')),
+                ('feed', models.URLField()),
+                ('last_update', models.DateTimeField(null=True, blank=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('content.category',),
+        ),
+        migrations.CreateModel(
+            name='WikiArticle',
+            fields=[
+                ('article_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='content.Article')),
+                ('identifier', models.CharField(max_length=255)),
+                ('identifier_type', models.CharField(default=b'title', max_length=6, blank=True, choices=[(b'pageid', b'pageid'), (b'title', b'title')])),
+            ],
+            options={
+                'abstract': False,
+            },
+            bases=('content.article',),
+        ),
+        migrations.CreateModel(
+            name='WikiCategory',
+            fields=[
+                ('category_ptr', models.OneToOneField(parent_link=True, auto_created=True, primary_key=True, serialize=False, to='content.Category')),
+                ('identifier', models.CharField(max_length=255)),
+                ('identifier_type', models.CharField(default=b'title', max_length=6, blank=True, choices=[(b'pageid', b'pageid'), (b'title', b'title')])),
+                ('wiki_type', models.CharField(default=b'14', max_length=3, choices=[(b'0', b'Page'), (b'14', b'Category'), (b'100', b'Portal')])),
+            ],
+            options={
+                'verbose_name_plural': 'WikiCategories',
+            },
+            bases=('content.category',),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='parent',
+            field=models.ForeignKey(related_name='children', blank=True, to='content.Category', null=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='polymorphic_ctype',
+            field=models.ForeignKey(related_name='polymorphic_content.category_set+', editable=False, to='contenttypes.ContentType', null=True),
+        ),
+        migrations.AddField(
+            model_name='category',
+            name='source',
+            field=models.ForeignKey(blank=True, to='content.ContentSource', null=True),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='categories',
+            field=models.ManyToManyField(related_name='articles', to='content.Category'),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='polymorphic_ctype',
+            field=models.ForeignKey(related_name='polymorphic_content.article_set+', editable=False, to='contenttypes.ContentType', null=True),
+        ),
+        migrations.AddField(
+            model_name='article',
+            name='source',
+            field=models.ForeignKey(blank=True, to='content.ContentSource', null=True),
+        ),
+        migrations.CreateModel(
+            name='KidsWeekCategory',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('content.rsscategory',),
+        ),
+        migrations.CreateModel(
+            name='SevenDaysCategory',
+            fields=[
+            ],
+            options={
+                'proxy': True,
+            },
+            bases=('content.rsscategory',),
+        ),
+    ]
