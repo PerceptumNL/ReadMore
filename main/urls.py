f8b1d3f63a62f09010c416007729555941112e8e
mslatour <sanderlatour@gmail.com>
2015-09-11 17:59:21 +0200

    Added forgot-password functionality.

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,20 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth import views as auth_views
 
 urlpatterns = patterns('',
+    url(r'^password_reset/$', auth_views.password_reset,
+        {'template_name': 'account/password_reset_form.html'},
+        name="password_reset"),
+    url(r'^password_reset/done/$', auth_views.password_reset_done,
+        {'template_name': 'account/password_reset_done.html'},
+        name="password_reset_done"),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm,
+        {'template_name': 'account/password_reset_confirm.html'},
+        name="password_reset_confirm"),
+    url(r'^reset/done/$', auth_views.password_reset_complete,
+        {'template_name': 'account/password_reset_complete.html'},
+        name='password_reset_complete'),
     url(r'^accounts/pilotsignup/$', 'main.views.pilot_signup', name='pilot_signup'),
     url(r'^accounts/profile/$', 'main.views.profile_self', name='profile'),
     url(r'^accounts/', include('allauth.urls')),
